4b1d5a0c6d685ffb6d512e612f13ad4ef4f3f365
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2021-07-07 02:02:46 +0530

    Final Commit

--- src/test/java/interviewPrep/CompareArray.java
+++ src/test/java/interviewPrep/CompareArray.java
@@ -1,7 +1,5 @@
 package interviewPrep;
-
 public class CompareArray {
-
 	public static void main(String[] args) {
 		int[] a= {1,4,5,7};
 		int[] b= {6,4,3,7};
@@ -9,9 +7,6 @@ public class CompareArray {
 		
 		
 		
-		
-		
-
+		System.out.println("Mrinmoy Biswas");
 	}
-
 }
