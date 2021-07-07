4d5ebbfb2953eb0ca09912f197dc6cd80be67a49
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-07 13:43:54 +0530

    DD

--- src/test/java/interviewPrep/CompareArray.java
+++ src/test/java/interviewPrep/CompareArray.java
@@ -1,12 +1,24 @@
 package interviewPrep;
+import java.util.ArrayList;
 public class CompareArray {
 	public static void main(String[] args) {
+		// Compare two different Array and create new Array with the matching values
 		int[] a= {1,4,5,7};
 		int[] b= {6,4,3,7};
-		
-		
-		
-		
-		System.out.println("Mrinmoy Biswas");
+		ArrayList<Integer>al=new ArrayList<Integer>();
+		for(int i=0; i<a.length;i++)
+		{
+			if(a[i]==b[i])
+			{
+				al.add(a[i]);
+			}
+		}
+		System.out.println(al);		
+		Object[] c=al.toArray();
+		//This is extended form of for loop
+		for(Object obj : c)
+		{
+			System.out.println(obj);
+		}
 	}
 }
