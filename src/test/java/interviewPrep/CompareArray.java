d49de11f2128c97eaa4480545b26c9bf7e525342
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-07 15:59:07 +0530

    ASD

--- src/test/java/interviewPrep/CompareArray.java
+++ src/test/java/interviewPrep/CompareArray.java
@@ -1,23 +1,22 @@
 package interviewPrep;
+
 import java.util.ArrayList;
+
 public class CompareArray {
 	public static void main(String[] args) {
 		// Compare two different Array and create new Array with the matching values
-		int[] a= {1,4,5,7};
-		int[] b= {6,4,3,7};
-		ArrayList<Integer>al=new ArrayList<Integer>();
-		for(int i=0; i<a.length;i++)
-		{
-			if(a[i]==b[i])
-			{
+		int[] a = { 1, 4, 5, 7 };
+		int[] b = { 6, 4, 3, 7 };
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == b[i]) {
 				al.add(a[i]);
 			}
 		}
-		System.out.println(al);		
-		Object[] c=al.toArray();
-		//This is extended form of for loop
-		for(Object obj : c)
-		{
+		System.out.println(al);
+		Object[] c = al.toArray();
+		// This is extended form of for loop
+		for (Object obj : c) {
 			System.out.println(obj);
 		}
 	}
---
+++ src/test/java/interviewPrep/Testppiramid.java
@@ -0,0 +1,17 @@
+package interviewPrep;
+
+public class Testppiramid {
+	public static void main(String[] args) {
+//		*
+//		* *
+//		* * *
+//		* * * *
+		for (int i = 0; i < 10; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+				System.out.print("  ");
+			}
+			System.out.println("");
+		}
+	}
+}
