4be7ca84b5da70424e0b1287f1e7cd472c6bb8e1
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-08 02:05:36 +0530

    New Commit

--- src/test/java/interviewPrep/FibonacciSeries.java
+++ src/test/java/interviewPrep/FibonacciSeries.java
@@ -1,12 +1,17 @@
 package interviewPrep;
-
 public class FibonacciSeries {
-
 	public static void main(String[] args) {
-		
-		
-		
-
+		// Printing the Fibonacci Series
+		int a = 0;
+		int b = 1;
+		int sum = 0;
+		System.out.print(a+" ");
+		for (int i = 0; i < 10; i++) 
+		{
+			sum = a + b;
+			a = b;
+			b = sum;
+			System.out.print(b+" ");
+		}
 	}
-
 }
---
+++ src/test/java/interviewPrep/RemoveDuplicateNumberCollection.java
@@ -0,0 +1,12 @@
+package interviewPrep;
+
+public class RemoveDuplicateNumberCollection {
+
+	public static void main(String[] args) {
+
+		int[] a = { 4, 5, 5, 5, 4, 6, 6, 9, 4 };
+		
+
+	}
+
+}
