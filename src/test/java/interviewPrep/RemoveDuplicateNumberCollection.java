5397b7a35231faf66953d02042c72b42701b4bea
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-08 09:14:40 +0530

    New Commit

--- src/test/java/interviewPrep/RemoveDuplicateNumberCollection.java
+++ src/test/java/interviewPrep/RemoveDuplicateNumberCollection.java
@@ -1,10 +1,44 @@
 package interviewPrep;
 
+import java.util.ArrayList;
+
 public class RemoveDuplicateNumberCollection {
 
 	public static void main(String[] args) {
-
+		// Print unique number from the list
 		int[] a = { 4, 5, 5, 5, 4, 6, 6, 9, 4 };
+		
+		int flag=0;
+		ArrayList<Integer>al=new ArrayList<Integer>();
+		for(int i=0;i<a.length;i++)
+		{
+			int k=0;
+			if(!al.contains(a[i])) {
+				al.add(a[i]);
+				k++;
+			}
+			
+			for(int j=i+1; j<a.length;j++)
+			{
+				if(a[i]==a[j])
+				{
+					k++;
+				}
+				
+				
+				
+			}
+			System.out.println(a[i]);
+			System.out.println(k);
+			
+		}
+		
+		
+//		for(Integer a1:al)
+//		{
+//			System.out.println(a1);
+//		}
+//		
 		
 
 	}
