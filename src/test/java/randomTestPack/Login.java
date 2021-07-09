0baf0d9765d552e1495b9102f6c78ad281362085
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2021-07-09 08:41:05 +0530

    First Commit 1

--- src/test/java/randomTestPack/Login.java
+++ src/test/java/randomTestPack/Login.java
@@ -1,19 +1,24 @@
 package randomTestPack;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.browser.Browser;
 import org.testng.annotations.Test;
+
 public class Login {
 	@Test
-	public void LoginTC() throws InterruptedException
-	{
-		System.setProperty("webdriver.chrome.driver", "/Users/Mrinmoy/Documents/Study_Materials/Eclipse_Projects/Selenium_Drivers/chromedriver"); 
-		WebDriver driver=new ChromeDriver();  
-		driver.navigate().to("https://www.facebook.com//");  
-		driver.manage().window().maximize(); 	
-		LoginPageObjectEncapsulation rd=new LoginPageObjectEncapsulation(driver);
+	public void LoginTC() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/Mrinmoy/Documents/Study_Materials/Eclipse_Projects/Selenium_Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.facebook.com//");
+		driver.manage().window().maximize();
+		LoginPageObjectEncapsulation rd = new LoginPageObjectEncapsulation(driver);
 		rd.EmailID().sendKeys("Hello");
 		rd.Password().sendKeys("Hello");
-		
+		Thread.sleep(5000);
+		// driver.quit();
+		driver.close();
+
 	}
 }
